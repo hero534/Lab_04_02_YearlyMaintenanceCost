61789fc0c3aafcbc786ff4a9801cdf17ca644651
hgold <goldsmhr@mail.uc.edu>
2025-02-03 20:37:36 -0500

    Task 02 Complete

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inOne = new Scanner(System.in);
-        Scanner inTwo = new Scanner(System.in);
-        Scanner inThree = new Scanner(System.in);
-        Scanner inFour = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         double springCost = 0.00;
         double summerCost = 0.00;
         double fallCost = 0.00;
         double winterCost = 0.00;
         double totalCost = 0.00;
             System.out.println("What is your home maintenance costs for Spring?");
-                springCost = inOne.nextDouble();
-            System.out.println("What is your home maintenance costs for Spring?");
-                summerCost = inTwo.nextDouble();
-            System.out.println("What is your home maintenance costs for Spring?");
-                fallCost = inThree.nextDouble();
-            System.out.println("What is your home maintenance costs for Spring?");
-                winterCost = inFour.nextDouble();
+                springCost = in.nextDouble();
+            System.out.println("What is your home maintenance costs for Summer?");
+                summerCost = in.nextDouble();
+            System.out.println("What is your home maintenance costs for Fall?");
+                fallCost = in.nextDouble();
+            System.out.println("What is your home maintenance costs for Winter?");
+                winterCost = in.nextDouble();
                 totalCost = springCost + summerCost + fallCost + winterCost;
             System.out.println("The sum of " + springCost + " " + summerCost + " " + fallCost + " " + winterCost + " is "+ totalCost);
     }
